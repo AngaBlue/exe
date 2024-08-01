d0c71a2c613cb0e88273c26f652c3771e2c260cb
AngaBlue <contact@anga.blue>
2024-08-01 23:20:47 +1000

    feat: display usage when config not found

--- src/cli.ts
+++ src/cli.ts
@@ -2,16 +2,36 @@ import { resolve } from 'path';
 import { readFile } from 'fs/promises';
 import exe from '.';
 
+declare interface Error {
+    name: string;
+    message: string;
+    stack?: string;
+    code?: number | string;
+}
+
+/**
+ * Exit the process with an error message and usage.
+ * @param err The error message to display.
+ */
+function exitWithError(err: string) {
+    console.error(err);
+    console.error('');
+    console.error('Usage: exe [exe.json]');
+    process.exit(1);
+}
+
 (async function main() {
     const args = process.argv.splice(2);
-    if (args.length !== 1) {
-        console.log('Usage: exe <exe.json>');
-        process.exit(1);
+
+    try {
+        const exeJsonPath = resolve(args[0] || 'exe.json');
+        const exeJson = await readFile(exeJsonPath);
+        const exeConfig = JSON.parse(String(exeJson));
+        await exe(exeConfig);
+    } catch (err) {
+        if ((err as Error).code === 'ENOENT') return exitWithError(`File not found: ${args[0]}`);
+        return exitWithError((err as Error).message);
     }
 
-    const exeJsonPath = resolve(args[0]);
-    const exeJson = await readFile(exeJsonPath);
-    const exeConfig = JSON.parse(String(exeJson));
-    await exe(exeConfig);
     process.exit(0);
 })();
